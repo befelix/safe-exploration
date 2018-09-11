24f3c6e0b704e9599b553dfba899a1262f460909
Torsten Koller <kollert@informatik.uni-freiburg.de>
2018-09-11 16:59:07 +0200

    Added gradient checker for dynamics

--- gp_ellipsoidal_reachability/test/test_environments.py
+++ gp_ellipsoidal_reachability/test/test_environments.py
@@ -5,17 +5,20 @@ Created on Mon Dec  4 13:24:39 2017
 @author: tkoller
 """
 import numpy as np
-from ..environments import InvertedPendulum
+import pytest
+from ..environments import InvertedPendulum, CartPole
 from ..utils import sample_inside_polytope
+from scipy.optimize import approx_fprime
 
-def before_test_inv_pend():
-    env = InvertedPendulum()
+@pytest.fixture(params=[InvertedPendulum(),CartPole()])
+def before_test_inv_pend(request):
+    env = request.param
     
     return env
 
-def test_normalization():
+def test_normalization(before_test_inv_pend):
     """ """
-    env = before_test_inv_pend()
+    env = before_test_inv_pend
     state = np.random.rand(2)
     action = np.random.rand(1)
     s_1,a_1 =  env.normalize(*env.unnormalize(state,action))
@@ -23,15 +26,12 @@ def test_normalization():
     assert np.all(s_1==state) 
     assert np.all(a_1==action)
     assert np.all(s_2==state)
-    assert np.all(a_2==action)
+    assert np.all(a_2==action)    
     
-def test_linearization():
+def test_safety_bounds_normalization(before_test_inv_pend):
     """ """
     
-def test_safety_bounds_normalization():
-    """ """
-    
-    env = before_test_inv_pend()
+    env = before_test_inv_pend
     
     n_samples = 50
     x = np.random.randn(n_samples,env.n_s)
@@ -43,3 +43,20 @@ def test_safety_bounds_normalization():
     in_norm = sample_inside_polytope(x_norm,h_mat_safe_norm,h_safe_norm)
     
     assert np.all(in_unnorm == in_norm), "do the normalized constraint correspond to the unnormalized?"
+
+
+
+def test_gradients(before_test_inv_pend):
+
+    env = before_test_inv_pend
+    n_s = env.n_s
+    n_u = env.n_u
+
+    for i in range(n_s):
+        f = lambda z: env._dynamics(0,z[:env.n_s],z[env.n_s:])[i]
+        f_grad = env._jac_dynamics()[i,:]
+        grad_finite_diff = approx_fprime(np.zeros((n_s+n_u,)),f,1e-8)
+
+        #err = check_grad(f,f_grad,np.zeros((n_s+n_u,)))
+
+        assert np.allclose(f_grad,grad_finite_diff), 'Is the gradient of the {}-th dynamics dimension correct?'.format(i)
