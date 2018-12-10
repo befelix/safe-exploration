dd034ec8ae8b948557d308d5012457b7c89f528a
Torsten Koller <kollert@informatik.uni-freiburg.de>
2018-12-10 10:51:55 +0100

    moved old safempc.py to old_files, improved docs

--- gp_ellipsoidal_reachability/run.py
+++ gp_ellipsoidal_reachability/run.py
@@ -29,25 +29,61 @@ def create_parser():
                         Default scenario is static_mpc_exploration.py (see above for explanation) """)
     return parser
     
+def check_config_conflicts(conf):
+    """ Check if there are conflicting options in the Config
+    
+    Parameters
+    ----------
+    conf: Config
+        The config file
+
+    Returns
+    -------
+    has_conflict: Bool
+        True, if there is a conflict in the config
+    conflict_str: String
+        The error message
+
+    """
+
+    has_conflict = False
+    conflict_str = ""
+    if conf.task == "exploration" and not conf.solver_type == "safempc":
+        return True, "Exploration task only allowed with safempc solver" 
+    elif conf.task == "uncertainty_propagation" and not conf.solver_type == "safempc":
+        return True ,"Uncertainty propagation task only allowed with safempc solver"
+
+    return has_conflict, conflict_str
+
 def run_scenario(args):
-    """ Run the specified scenario """    
+    """ Run the specified scenario 
+
+    Parameters
+    ----------
+    args: 
+        The parsed arguments (see create_parser for details)
+    """    
     config_path = args.scenario_config
     conf = loadConfig(config_path)
 
-    env = create_env(conf.env_name,conf.env_options)
-    gp_model_options = get_model_options_from_conf(conf,env)
+    conflict, conflict_str = check_config_conflicts(conf)
+    if conflict:
+        raise ValueError("There are conflicting settings: {}".format(conflict_str))
 
-    safempc = create_solver(conf,env,gp_model_options)
+    env = create_env(conf.env_name,conf.env_options)
+
+    gp_model_options = get_model_options_from_conf(conf,env)
+    solver, safe_policy = create_solver(conf,env,gp_model_options)
     
     task = conf.task
     if task == "exploration":
-        run_exploration(env,safempc,conf,conf.static_exploration,conf.n_iterations,
-                        conf.n_restarts_optimizer,conf.visualize,conf.save_vis,conf.save_path)
+        run_exploration(conf)#,conf.static_exploration,conf.n_iterations,
+                        #conf.n_restarts_optimizer,conf.visualize,conf.save_vis,conf.save_path,conf.verify_safety,conf.n_experiments)
     elif task == "episode_setting":
-        run_episodic(conf,safempc)
+        run_episodic(conf)
         
     elif task == "uncertainty_propagation":
-        run_uncertainty_propagation(env,safempc,conf)
+        run_uncertainty_propagation(env,solver,conf)
         
 if __name__ == "__main__":
     parser = create_parser()
