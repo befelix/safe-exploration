4f6aabdb308925441bf475c028ccac4f24a7b8e0
Torsten Koller <kollert@informatik.uni-freiburg.de>
2019-01-14 17:08:09 +0100

    Fixed seed + slightly increased tolerance

--- safe_exploration/test/test_gp_models.py
+++ safe_exploration/test/test_gp_models.py
@@ -11,14 +11,19 @@ import numpy as np
 from casadi import Function, SX
 from ..gp_models import SimpleGPModel
 
-@pytest.fixture(params = [("InvPend",["rbf","rbf"]),("InvPend",["lin_rbf","lin_rbf"])])
+np.random.seed(125)
+a_tol = 1e-6
+r_tol = 1e-4
+
+@pytest.fixture(params = [("InvPend",["rbf","rbf"]),("InvPend",["lin_rbf","lin_rbf"]),("InvPend",["lin_mat52","lin_mat52"])])
 def before_gp_predict_test(request):
     
     env, kern_types = request.param
     if env == "InvPend":
+        path = "invpend_data.npz"
         n_s = 2
         n_u = 1
-        path = "invpend_data.npz"
+
         
     train_data = np.load(path)
     X = train_data["X"]
@@ -42,8 +47,8 @@ def test_predict_casadi_symbolic(before_gp_predict_test):
     out_cas = f_nograd(test_input.T)
     out_numeric = gp.predict(test_input.T)
     
-    assert np.all(np.isclose(out_cas[0],out_numeric[0])), "Do the predictive means match?"
-    assert np.all(np.isclose(out_cas[1],out_numeric[1])), "Do the predictive vars match?"
+    assert np.allclose(out_cas[0],out_numeric[0],r_tol,a_tol), "Do the predictive means match?"
+    assert np.allclose(out_cas[1],out_numeric[1],r_tol,a_tol), "Do the predictive vars match?"
     
     
     
