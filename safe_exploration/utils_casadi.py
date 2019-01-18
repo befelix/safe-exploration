6d428e81eb443b8f8deb2246f75c3af893960145
Torsten Koller <kollert@informatik.uni-freiburg.de>
2019-01-18 20:26:40 +0100

    removing whitespaces

--- safe_exploration/utils_casadi.py
+++ safe_exploration/utils_casadi.py
@@ -4,7 +4,7 @@
 A set of utility functions required by various different modules / classes.
 This module contains a subset of the functions implemented in the corresponding
 utils.py file with the same functionality but is implemented in a way that
-admits being use by a Casadi ( symbolic ) framework. 
+admits being use by a Casadi ( symbolic ) framework.
 
 @author: tkoller
 """
@@ -16,7 +16,7 @@ import numpy as np
 
 def compute_bounding_box_lagrangian(q,L,K,k,order = 2, verbose = 0):
     """ Compute box to overapproximate lagrangian remainder for Casadi
-    
+
     Parameters
     ----------
     q: n_s x n_s array,
@@ -25,7 +25,7 @@ def compute_bounding_box_lagrangian(q,L,K,k,order = 2, verbose = 0):
         Set of Lipschitz constants
     K: n_u x n_s array,
             The state feedback-matrix for the controls
-    k: n_u x n-u array, 
+    k: n_u x n-u array,
             The additive term of the controls
     order: int, optional
         The order of the taylor remainder term
@@ -35,33 +35,33 @@ def compute_bounding_box_lagrangian(q,L,K,k,order = 2, verbose = 0):
     SUPPORTED_TAYLOR_ORDER = [1,2]
     if not (order in SUPPORTED_TAYLOR_ORDER):
         raise ValueError("Cannot compute lagrangian remainder bounds for the given order")
-    
+
     if order == 2:
         s_max = matrix_norm_2(q)
 
         qk = mtimes(K,mtimes(q,K.T))
         sk_max = matrix_norm_2(qk)
 
-        l_max = s_max**2 + sk_max**2 
+        l_max = s_max**2 + sk_max**2
         box_lower = -L*l_max * 0.5
         box_upper =  L*l_max * 0.5
-        
+
     if order == 1:
         s_max = matrix_norm_2(q)
 
         qk = mtimes(K,mtimes(q,K.T))
         sk_max = matrix_norm_2(qk)
         l_max = s_max + sk_max
-        
-        box_lower = -L*l_max 
-        box_upper =  L*l_max 
-        
+
+        box_lower = -L*l_max
+        box_upper =  L*l_max
+
     return box_lower, box_upper
-    
+
 
 def compute_remainder_overapproximations(q,k_fb,l_mu,l_sigma):
-    """ Compute symbolically the (hyper-)rectangle over-approximating the lagrangians of mu and sigma 
-    
+    """ Compute symbolically the (hyper-)rectangle over-approximating the lagrangians of mu and sigma
+
     Parameters
     ----------
     q: n_s x n_s ndarray[casadi.SX.sym]
@@ -87,99 +87,99 @@ def compute_remainder_overapproximations(q,k_fb,l_mu,l_sigma):
     qb = mtimes(q,b)
     evals = matrix_norm_2_generalized(b,q)
     r_sqr = vec_max(evals)
-    
+
     u_mu = l_mu*r_sqr
     u_sigma = l_sigma*sqrt(r_sqr)
-    
+
     return u_mu, u_sigma
-    
+
 
 def vec_max(x):
     """ Compute (symbolically) the maximum element in a vector
-    
+
     Parameters
     ----------
-    x : nx1 or array 
+    x : nx1 or array
         The symbolic input array
     """
     n,_ = x.shape
-    
-    if n == 1: 
-        return x[0]   
+
+    if n == 1:
+        return x[0]
     c = fmax(x[0],x[1])
     if n > 2:
         for i in range(1,n-1):
             c = fmax(c,x[i+1])
     return c
-    
+
 
 def matrix_norm_2_generalized(a, b_inv, x = None, n_iter = None):
     """ Get largest generalized eigenvalue of the pair inv_a^{-1},b
-    
-    get the largest eigenvalue of the generalized eigenvalue problem 
-        a x = \lambda b x 
-    <=> b x = (1/\lambda) a x 
-    
+
+    get the largest eigenvalue of the generalized eigenvalue problem
+        a x = \lambda b x
+    <=> b x = (1/\lambda) a x
+
     Let \omega := 1/lambda
-    
-    We solve the problem 
-        b x = \omega a x 
+
+    We solve the problem
+        b x = \omega a x
     using the inverse power iteration which converges to
     the smallest generalized eigenvalue \omega_\min
-    
+
     Hence we can get  \lambda_\max = 1/\omega_\min,
         the largest eigenvalue of a x = \lambda b x
-        
+
     """
     n,_ = a.shape
     if x is None:
         x = np.eye(n,1)
         x /= norm_2(x)
-        
+
     if n_iter is None:
         n_iter = 2*n**2
-        
+
     y = mtimes(b_inv,mtimes(a,x))
     for i in range(n_iter):
         x = y/norm_2(y)
         y = mtimes(b_inv,mtimes(a,x))
-        
+
     return mtimes(y.T,x)
-    
-    
+
+
 def matrix_norm_2(a_mat,x = None,n_iter = None):
     """ Compute an approximation to the maximum eigenvalue of the hermitian matrix x
-       
+
     TODO: Can we impose a convergence constraint?
-    TODO: Check for diagonal matrix 
-    
-    """        
-    n,m = a_mat.shape  
+    TODO: Check for diagonal matrix
+
+    """
+    n,m = a_mat.shape
     assert n == m, "Input matrix has to be symmetric"
-    
+
     if x is None:
         x = np.eye(n,1)
         x /= norm_2(x)
-        
+
     if n_iter is None:
         n_iter = 2*n**2
-    
+
     y = mtimes(a_mat,x)
-    
+
     for i in range(n_iter):
         x = y / norm_2(y)
         y = mtimes(a_mat,x)
-    
+
     return mtimes(y.T,x)
-        
-    
+
+
 def trig_prop(m, v, idx , a = 1.0):
     """ Exact moment-matching for trig function with Gaussian input
 
     Compute E(a*sin(x)), E(a*cos(x)), V(a*sin(x)), V(a*cos(x)) and cross-covariances
     for Gaussian inputs x \sim N(m_idx,v_idx) as well as input-output covariances
     using exact moment-matching
-    
+
     Parameters
     ----------
     m : dx1 ndarray[float | casadi.Sym]
@@ -188,13 +188,13 @@ def trig_prop(m, v, idx , a = 1.0):
     idx: int
         The index to be trigonometrically augmented
     a: float [optional]
-        A scalar coefficient 
+        A scalar coefficient
 
     Returns
     -------
     m_out: 2x1 ndarray[float | casadi.Sym]
         The mean of the trigonometric output
-    v_out: 2x2 ndarray[float | casadi.Sym] 
+    v_out: 2x2 ndarray[float | casadi.Sym]
         The variance of the trigonometric output
     c_out: inv(v) times input-output-covariance
 
@@ -209,7 +209,7 @@ def trig_prop(m, v, idx , a = 1.0):
 
     m_out[0] = a*m_sin
     m_out[1] = a*m_cos
-    
+
     v_exp_1 = exp(-2*v[idx,idx])
     e_s_sq = (1 - v_exp_1*cos(2*m[idx,0]))/2
     e_c_sq = (1 + v_exp_1*cos(2*m[idx,0]))/2
@@ -242,7 +242,7 @@ def trig_aug(m,v,idx,a = 1.0, keep_radian = False):
     idx: int
         The index to be trigonometrically augmented
     a: float [optional]
-        A scalar coefficient 
+        A scalar coefficient
     keep_radian: bool [optional]
         Should the original representation (radian) of the angle be kept?
 
@@ -251,7 +251,7 @@ def trig_aug(m,v,idx,a = 1.0, keep_radian = False):
     -------
     m_out: (d+2)x1 ndarray[float | casadi.Sym]
         The mean of the gaussian augmented with sine/cosine
-    v_out: (d+1)x(d+1) or (d+2)x(d+2) ndarray[float | casadi.Sym] 
+    v_out: (d+1)x(d+1) or (d+2)x(d+2) ndarray[float | casadi.Sym]
         The variance of the gaussian augmented with since cosine
 
     """
@@ -302,11 +302,11 @@ def generic_cost(mu,sigma, u, step_cost, terminal_cost, state_trafo = None, lamb
     mu_T,v_T = state_trafo(mu_T,v_T)
     c += terminal_cost(mu_T,v_T)
 
-    return c 
+    return c
 
 def cost_dev_safe_perf(m_safe,m_perf,W = None):
     """ quadratic cost function on the deviation between safety and performance traejctory"""
-    
+
     n_safe, n_s = np.shape(m_safe)
     n_perf, _ = np.shape(m_safe)
 
@@ -320,7 +320,7 @@ def cost_dev_safe_perf(m_safe,m_perf,W = None):
     return cost
 
 def loss_sat(m,v,z,W = None):
-    """ Saturating cost function 
+    """ Saturating cost function
 
     Parameters
     ----------
@@ -337,11 +337,11 @@ def loss_sat(m,v,z,W = None):
     L: float
         The expected loss under the saturating cost function
 
-    Warning: Solving the Matlab system W/(eye(D)+SW) via inversion. Can be instable 
+    Warning: Solving the Matlab system W/(eye(D)+SW) via inversion. Can be instable
     TO-DO: Should be fixed
 
     """
-    D = np.shape(m)[0] 
+    D = np.shape(m)[0]
 
 
     if W is None:
@@ -358,7 +358,7 @@ def loss_sat(m,v,z,W = None):
     return 1+L
 
 def loss_quadratic(m,z,v = None, W = None):
-    """ Quadratic cost function 
+    """ Quadratic cost function
 
     Simple quadratic loss with W as weight matrix, ignoring variance
     Parameters
@@ -374,10 +374,10 @@ def loss_quadratic(m,z,v = None, W = None):
     Returns
     -------
     L: float
-        The quadratic loss 
+        The quadratic loss
     """
-    
-    D = np.shape(m)[0] 
+
+    D = np.shape(m)[0]
 
 
     if W is None:
