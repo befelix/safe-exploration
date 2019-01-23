7f1409a363d377458b200413e3ef8a5e5e2aec57
Torsten Koller <kollert@informatik.uni-freiburg.de>
2019-01-23 17:06:31 +0100

    Initial version of the SSM base class / interface and the CasadiSSMEvaluator which generates a casadi function from the SSM. Also included initial version of a reimplementation of the GP written with casadi functions to follow the SSM interface

--- safe_exploration/test/test_utils.py
+++ safe_exploration/test/test_utils.py
@@ -8,19 +8,19 @@ import numpy as np
 from ..utils import sample_inside_polytope
 
 def test_sample_inside_polytope():
-    """ 
-    
+    """
+
     polytope:
         -.3 < x_1 < .4
         -.2 < x_2 < .2
     """
-    x = np.array([[0.1,0.15],[0.0,0.0],[.5,.15]]) 
+    x = np.array([[0.1,0.15],[0.0,0.0],[.5,.15]])
 
     a = np.vstack((np.eye(2),-np.eye(2),-np.eye(2)))
     b = np.array([.4,.2,.3,.2,.3,.2])[:,None]
-    
+
     res = sample_inside_polytope(x,a,b)
 
     res_expect = np.array([True,True,False])#should be: inside, inside, not inside
-    
+
     assert np.all(res == res_expect), "Are the right samples inside/outside the polyope?"
