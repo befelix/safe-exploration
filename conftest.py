753e09012491031d0eef266579182ec596d811b8
Torsten Koller <kollert@informatik.uni-freiburg.de>
2019-02-07 13:12:35 +0100

    Append /ssm_pytorch to the collect_ignore list in case the dependencies are not installed

--- conftest.py
+++ conftest.py
@@ -8,6 +8,15 @@ try:
 except:
     _has_ssm_gpy_ = False
 
+try:
+    import safe_exploration.ssm_pytorch
+    safe_exploration.ssm_pytorch.__name__ #Need this, otherwise we get an unused import error
+    _has_ssm_pytorch_ = True
+except:
+    _has_ssm_pytorch_ = False
+
 collect_ignore = ["setup.py"]
 if not _has_ssm_gpy_:
     collect_ignore.append("safe_exploration/ssm_gpy")
+if not _has_ssm_pytorch_:
+    collect_ignore.append("safe_exploration/ssm_pytorch")
